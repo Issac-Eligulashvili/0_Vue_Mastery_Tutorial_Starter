268ee612e1f2c4d5ea63882fc292d13e7aca5332
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-12-12 14:35:44 -0500

    Computed Properties

--- main.js
+++ main.js
@@ -2,22 +2,35 @@ const app = Vue.createApp({
      data() {
           return {
                product: 'Socks',
-               image: './assets/images/socks_green.jpg',
-               inventory: 11,
                details: ["50% cotton", "30% wool", "20% polyester"],
+               selectedVariant: 0,
                variants: [
-                    {id: 2234, color: "green", image: './assets/images/socks_green.jpg' },
-                    {id: 2235, color: "blue", image: './assets/images/socks_blue.jpg'}
+                    {id: 2234, color: "green", image: './assets/images/socks_green.jpg', quantity: 50 },
+                    {id: 2235, color: "blue", image: './assets/images/socks_blue.jpg', quantity: 0}
                ],
-               cart: 0
+               cart: 0,
+               brand: "Vue Mastery"
           }
      },
      methods: {
           addToCart() {
                this.cart++;
           },
-          updateImage(image) {
-               this.image = image;
+          updateVariant(index) {
+               this.selectedVariant = index;
+               console.log(this.variants[this.selectedVariant].image);
           }
+     },
+     computed: {
+          title() {
+               return this.brand + " " + this.product
+          },
+          image() {
+               
+               return this.variants[this.selectedVariant].image;
+          },
+          inStock() {
+               return this.variants[this.selectedVariant].quantity;
+          } 
      }
 })
